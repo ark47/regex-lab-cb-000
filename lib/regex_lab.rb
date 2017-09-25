b56cb59946b6b09e98ae8bc6f95c7c21554e9be8
Chris Nunez <cenunez47@gmail.com>
2017-09-25 01:56:31 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,5 +15,5 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  text.match()/(\+*\d{1,})*([ |\(])*(\d{3})[^\d]*(\d{3})[^\d]*(\d{4})/) ? true : false
+  text.match(/^[2-9]\d{2}-\d{3}-\d{4}$/) ? true : false
 end
